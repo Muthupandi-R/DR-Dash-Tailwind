a0cf4c1623293c09e51822493c9494ee31019979
Muthupandi-R <156410933+Muthupandi-R@users.noreply.github.com>
2025-07-07 11:35:24 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+import colors from 'tailwindcss/colors';
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: colors.indigo[600], // Enables usage like `bg-primary`
+          100: colors.indigo[100],
+          200: colors.indigo[200],
+          300: colors.indigo[300],
+          400: colors.indigo[400],
+          500: colors.indigo[500],
+          600: colors.indigo[600],
+          700: colors.indigo[700],
+          800: colors.indigo[800],
+        },
+      },
+    },
+  },
+  plugins: [],
+}
